5d081b468f0e7ce67b86bbd3bc9e326866e14c46
Edwin Chia <starlest@gmail.com>
2016-07-07 17:32:10 +0800

    Add 5 secs timeout to load the map

--- js/views/map-view.js
+++ js/views/map-view.js
@@ -1,5 +1,13 @@
 var map;
 
+$(function () {
+    setTimeout(function () {
+        if (!window.google || !window.google.maps) {
+            $('.map').text("Failed to load Google Maps.");
+        }
+    }, 5000);
+});
+
 function initMap() {
     map = new google.maps.Map(document.getElementsByClassName("map")[0], {
         center: {lat: 0, lng: 0},
